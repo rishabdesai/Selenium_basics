1daae75bbb9a7fda05ce0ce4e3f2ef52792b9645
rishabh desai <desai.rishab@gmail.com>
2021-02-04 14:03:21 +0530

    1st commit

---
+++ src/Demo.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		//invoke .exe file first
+		String key ="webdriver.chrome.driver";
+		String value="C:\\Users\\d_ris\\OneDrive\\903_testing\\chromedriver_win32\\chromedriver.exe";
+		System.setProperty(key, value);
+		
+		
+		//create driver object for chrome browser
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com");
+
+	}
+
+}
