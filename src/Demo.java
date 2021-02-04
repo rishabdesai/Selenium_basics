fe5ce1c0906edfccebff5a96570c26142814d5c6
rishabh desai <desai.rishab@gmail.com>
2021-02-04 16:59:22 +0530

    more methods added

--- src/Demo.java
+++ src/Demo.java
@@ -14,8 +14,37 @@ public class Demo {
 		//create driver object for chrome browser
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
+		driver.get("https://www.google.com"); // open the URL on the chrome browser
+		System.out.println(driver.getTitle());  //validate if the page title is correct 
+		
+		//to check if you are landed on correct url
+		System.out.println(driver.getCurrentUrl());
+		
+		//to check the page source (f12)
+		System.out.println(driver.getPageSource());
+		
+		//to navigate to another web page
+		driver.get("http://yahoo.com");
+		
+		//to mimic back button of browser
+		//driver.navigate().back();
+		//driver.navigate().forward();
+		
+		//to close the current browser
+		driver.close();
+		//to close all the browsers
+		driver.quit();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
