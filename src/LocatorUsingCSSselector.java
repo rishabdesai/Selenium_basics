0c1fa7e53c22f6005ce3ec96573413ca34fe0c54
rishabh desai <desai.rishab@gmail.com>
2021-02-04 18:43:34 +0530

    using cssSelector

--- src/LocatorUsingCSSselector.java
+++ src/LocatorUsingCSSselector.java
@@ -1,27 +1,28 @@
+
 /* This code will open the login.salesforce.com page using .get() method. 
- * Use findElement to locate username (using ID) and password (using name) field, enter some dummy data
+ * Use findElement and cssSelector to locate username and password field, enter some dummy data
  * use xpath to find the login button, then click it.
  */
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocatorUsingXpath {
+public class LocatorUsingCSSselector {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\d_ris\\OneDrive\\903_testing\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		//open salesforce page, select user email & password, enter dummy value in it.
+		// open salesforce page, select user email & password, enter dummy value in it.
 		driver.get("http://login.salesforce.com");
-		driver.findElement(By.id("username")).sendKeys("dummy@email.com");
-		driver.findElement(By.name("pw")).sendKeys("123456");
+		driver.findElement(By.cssSelector("#username")).sendKeys("dummy@email.com");
+		driver.findElement(By.cssSelector("#password")).sendKeys("123456");
 		// select login button using xpath, click it.
 		driver.findElement(By.xpath("//*[@id='Login']")).click();
-		
-		
+		// if login fails, then print the error message on console using getText()
+		System.out.println(driver.findElement(By.cssSelector("div#error.loginError")).getText());
+
 	}
 
 }
